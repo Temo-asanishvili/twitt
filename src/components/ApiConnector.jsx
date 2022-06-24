e380ede06513b9053f1cc915978f6be348314a5d
BLADWING <timoti.ponarenkO@gmail.com>
2022-06-24 23:38:30 +0400

    optimize API system,

---
+++ src/components/ApiConnector.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+
+export const useFetch = (url) => {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+      fetch(url)
+        .then((response) => response.json())
+        .then((json) => {
+          setData(json);
+        });
+    }, [url]);
+    return data;
+  };
+  export const TweetsApi = (id) => {
+    const url =
+      id === null
+        ? "https://jsonplaceholder.typicode.com/posts/"
+        : "https://jsonplaceholder.typicode.com/posts/" + id;
+  
+    const data = useFetch(url);
+    return data;
+  };
+  export const UserApi= (id) => {
+    const url = "https://jsonplaceholder.typicode.com/users?id=" + id;
+  
+    const data = useFetch(url);
+  
+    return { data };
+  };
+  export const ImgApi = (id) => {
+    const url = "https://jsonplaceholder.typicode.com/photos?id=" + id;
+  
+    const data = useFetch(url);
+  
+    return { data };
+  };
+  
+export const CommentsaApi = () => {
+    const url = "https://jsonplaceholder.typicode.com/comments";
+  
+    const data = useFetch(url);
+  
+    return data;
+  };
+  
