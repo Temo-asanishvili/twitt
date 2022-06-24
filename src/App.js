64b66d7f95c6549a1cf79bdb77bbeefaf389205b
BLADWING <timoti.ponarenkO@gmail.com>
2022-06-25 00:26:33 +0400

    correct style, try to fix Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
 import Tweets from "./components/Tweets";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from 'react-router-dom';
+
 
 
 
@@ -8,8 +14,22 @@ import "./App.scss";
 
 const App = () => {
   return (
+    <Router>
+      <Routes>
+     <Route path="posts/:id" component={<Detail/>}/>
+     </Routes>
    <Tweets/>
+     
+   </Router>
   );
 };
+
+
+function Detail () {
+  return (
+<div>this in detail information</div>
+  )
+    
+}
 
 export default App;
--- src/components/Tweets.jsx
+++ src/components/Tweets.jsx
@@ -1,24 +1,26 @@
 import React, { useState } from "react";
 import { TweetsApi, UserApi } from "./ApiConnector";
+import { Link } from "react-router-dom";
 
+export default function Tweets(props) {
+  const [tweetsId, setTweetsId] = useState(null);
+  const tweets = TweetsApi(tweetsId);
+  const users = UserApi(tweetsId);
+  const {tweet} = props;
+  return (
+    <div>
+      {tweets.map((tweet) => {
+        return (
+          <div className="TweetContainer" key={tweet.id}>
+            <h3> tweet title:</h3>
+            <p className="mainTweet">{tweet.title}</p>
 
-export default function Tweets () {
-    const [tweetsId, setTweetsId] = useState(null);
-    const tweets = TweetsApi(tweetsId)
-    const users = UserApi(tweetsId)
-    return (
-        <div>   
-            
-            {tweets.map((tweet) => {
-
-                return (
-                    <div key={tweet.id}>
-                    tweet  title:
-                     <p>{tweet.title}</p>
-                    </div>
-                )
-            })}
-            this is tweetss....
-        </div>
-    )
+            <Link to={`/${tweet.id}`}> read more... </Link>
+           
+          </div>
+        );
+      })}
+      this is tweetss....
+    </div>
+  );
 }
